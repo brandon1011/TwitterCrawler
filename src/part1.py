ffc42b14bd614505c613778289485e760b391ba2
Libo Wang <billikx@gmail.com>
2015-02-20 23:48:06 -0800

    Fixed part1 to produce result from only one hashtag. Also updated the result.

--- src/part1.py
+++ src/part1.py
@@ -6,6 +6,16 @@ import json
 import datetime, time
 import TweetGrabber
 
+def writeTweetsToFile(grabber, f):
+	while True:
+		nextTweet = grabber.nextTweet()
+		if nextTweet is not None:
+			f.write(json.dumps(nextTweet)+ '\n')
+		else:
+			break
+
+f_tweets = open('../result/top_tweets.txt', 'w')
+
 start_date = datetime.datetime(2015,02,01, 20,00,0)
 end_date = datetime.datetime(2015,02,02, 11,00,0)
 mintime = int(time.mktime(start_date.timetuple()))
@@ -13,14 +23,11 @@ maxtime = int(time.mktime(end_date.timetuple()))
 
 hashtags = ['#SB49', '#football', '#MakeSafeHappen', '#brandbowl', '#adbowl']
 
-for hashtag in hashtags:
-	grabber = TweetGrabber.TweetGrabber(hashtag, mintime, maxtime)
-	grabber.fetchTweet(5)
+# Let's only search for the first hashtag #SB49
+hashtag = hashtags[0]
+grabber = TweetGrabber.TweetGrabber(hashtag, mintime, maxtime)
+grabber.fetchTweet(5)
         
-	while True:
-		nextTweet = grabber.nextTweet()
-		if nextTweet is not None:
-			print(json.dumps(nextTweet))
-		else:
-			break
+writeTweetsToFile(grabber, f_tweets)
 
+f_tweets.close()
