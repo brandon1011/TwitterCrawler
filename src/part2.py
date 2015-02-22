368c4b7b422a941412a4d43a8e94b56f82d180dc
Moustafa Alzantot <malzantot@ucla.edu>
2015-02-21 22:17:29 -0800

    Fixed the search period maxtime inclusion rule

--- src/part2.py
+++ src/part2.py
@@ -6,8 +6,8 @@ import json
 import datetime, time
 import TweetGrabber
 
-start_date = datetime.datetime(2015,02,01, 20,00,0)
-end_date = datetime.datetime(2015,02,02, 11,00,0)
+start_date = datetime.datetime(2015,2,1, 20,0,0)
+end_date = datetime.datetime(2015,2,2, 11,0,0)
 mintime = int(time.mktime(start_date.timetuple()))
 maxtime = int(time.mktime(end_date.timetuple()))
 
@@ -16,18 +16,18 @@ hashtags = ','.join(hashtags_list)
 
 
 win_start = mintime	# start of time window of tweets to be crawled
-def_win_sz = 500	# default window size
+def_win_sz = 512	# default window size
 win_sz = def_win_sz	# Window size initially the default size
 
 f_tweets = open('../result/tweets.txt', 'w')
-f_searchlog = open('../result/searchlog.txt', 'w')
+f_searchlog = open('../result/search_log.txt', 'w')
 
-while (win_start < maxtime):
+while (win_start <= maxtime):
 	win_end = min(win_start + win_sz, maxtime)
-	grabber = TweetGrabber.TweetGrabber(hashtags, win_start, win_end)
+	grabber = TweetGrabber.TweetGrabber(hashtags, win_start, win_end+1)
 	grabber.fetchTweet(500)	# default fetch size is 500
 	results_count = grabber.countTweets()
-	if (results_count < 500):
+	if (results_count < 400):
 		########## Save tweets
 		while True:
 			nextTweet = grabber.nextTweet()
@@ -50,7 +50,7 @@ while (win_start < maxtime):
 		win_start = win_end + 1 
 		
 		########## Increase window size if too little results returned.
-		if (results_count < 125):
+		if (results_count < 100 and win_sz <= 256):
 			win_sz = min(def_win_sz, win_sz*2)
 			print("+++ Increasing window size ! new size = %d" %win_sz)
 
